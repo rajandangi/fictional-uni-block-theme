3b59f7fda695499f4f9513c527eaf031926e8765
rajandangi <rajandangi649@gmail.com>
2024-01-14 20:28:37 +1030

    Add placeholder block for Events and Blogs

--- wp-content/themes/fictional-block-theme/functions.php
+++ wp-content/themes/fictional-block-theme/functions.php
@@ -236,6 +236,41 @@ function makeNotePrivate($data, $postarr)
 }
 
 
+class PlaceholderBlock
+{
+	private $name;
+
+	function __construct($name)
+	{
+		$this->name = $name;
+		add_action('init', [$this, 'onInit']);
+	}
+
+	function ourRenderCallback($attributes, $content)
+	{
+		ob_start();
+		require get_theme_file_path("/our-blocks/{$this->name}.php");
+		return ob_get_clean();
+	}
+
+	function onInit()
+	{
+		wp_register_script(
+			$this->name,
+			get_stylesheet_directory_uri() . "/our-blocks/{$this->name}.js",
+			array('wp-blocks', 'wp-editor')
+		);
+
+		register_block_type(
+			"ourblocktheme/{$this->name}",
+			array(
+				'editor_script' => $this->name,
+				'render_callback' => [$this, 'ourRenderCallback']
+			)
+		);
+	}
+}
+new PlaceholderBlock('eventsandblogs');
 
 class JSXBlock
 {
@@ -283,3 +318,5 @@ class JSXBlock
 new JSXBlock('banner', true);
 new JSXBlock('genericheading');
 new JSXBlock('genericbutton');
+
+
