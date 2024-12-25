a3633bd559644fd65a4b787718ccc4a616c4dc2f
rajandangi <rajandangi649@gmail.com>
2024-12-25 20:21:38 +1030

    Remove PlaceholderBlock class

--- wp-content/themes/fictional-block-theme/functions.php
+++ wp-content/themes/fictional-block-theme/functions.php
@@ -235,41 +235,6 @@ function makeNotePrivate($data, $postarr)
 }
 
 
-class PlaceholderBlock
-{
-    private $name;
-
-    function __construct($name)
-    {
-        $this->name = $name;
-        add_action('init', [$this, 'onInit']);
-    }
-
-    function ourRenderCallback($attributes, $content)
-    {
-        ob_start();
-        require get_theme_file_path("/our-blocks/{$this->name}.php");
-        return ob_get_clean();
-    }
-
-    function onInit()
-    {
-        wp_register_script(
-            $this->name,
-            get_stylesheet_directory_uri() . "/our-blocks/{$this->name}.js",
-            array('wp-blocks', 'wp-editor')
-        );
-
-        register_block_type(
-            "ourblocktheme/{$this->name}",
-            array(
-                'editor_script' => $this->name,
-                'render_callback' => [$this, 'ourRenderCallback']
-            )
-        );
-    }
-}
-
 // Register New blocks
 function fictional_university_blocks()
 {
